60dc71da88816f3b29819a7fe6f3c905181cd000
Tony Thomas <thoma127@umn.edu>
2012-10-09 20:10:51 -0500

    Minor cleanup of code formatting.

--- functions.php
+++ functions.php
@@ -196,14 +196,20 @@ function remove_category_rel_from_category_list($thelist)
 function add_slug_to_body_class($classes)
 {
     global $post;
-    if (is_home()) {
+    if (is_home())
+	{
         $key = array_search('blog', $classes);
-        if ($key > -1) {
+        if ($key > -1) 
+		{
             unset($classes[$key]);
         }
-    } elseif (is_page()) {
+    }
+	elseif (is_page())
+	{
         $classes[] = sanitize_html_class($post->post_name);
-    } elseif (is_singular()) {
+    }
+	elseif (is_singular())
+	{
         $classes[] = sanitize_html_class($post->post_name);
     }
 
